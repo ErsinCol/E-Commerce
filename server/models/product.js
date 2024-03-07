27af2940312f382769d3803ce808beaa18388b91
Ersin Çöl <ersincol235@gmail.com>
2024-03-07 16:30:47 +0300

    feat: Create product model

---
+++ server/models/product.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    photos: [String],
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+})
+
+const Product = mongoose.model("product", ProductSchema);
+
+export default Product;
