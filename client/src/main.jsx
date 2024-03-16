f9abc1f7f4fcfe54bfba1899ca7161bdc453520a
Ersin Çöl <ersincol235@gmail.com>
2024-03-16 11:13:04 +0300

    feat: Setup react-query-devtools

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,9 +5,17 @@ import ReactDOM from 'react-dom/client'
 import {ChakraProvider} from "@chakra-ui/react";
 import {RouterProvider} from "react-router-dom";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 import router from "./router/index.jsx";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnMount: false,
+            refetchOnWindowFocus: false,
+        }
+    }
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -15,6 +23,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <ChakraProvider>
               <RouterProvider router={router} />
           </ChakraProvider>
+          <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
   </React.StrictMode>,
 )
